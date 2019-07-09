87a4058dacb63ec1c79c6d55f56cbd4fb1c573f6
Ivan Shevchenko <rayserviz@gmail.com>
2019-07-09 11:37:31 +0300

    work hw

---
+++ homework_04/js/practical_07.js
@@ -0,0 +1,19 @@
+/*
+* Есть два массива names и prices с именами и ценами товаров.
+* Напишите функцию combine(names, prices), которая получает
+* эти два массива и возвращает массив объектов со свойствами name и price.
+*/
+
+const names = ['Радар', 'Сканер', 'Дроид', 'Захват', 'Двигатель', 'Топливный бак'];
+const prices = [1000, 2000, 1500, 2700, 1600, 8000];
+
+const products = function (names, prices) {
+  const combine = [];
+  for (name of names) {
+    console.log(name);
+    combine.push[names[0]];
+    combine.push[prices[1]];
+  }
+  return combine;
+};
+console.log(name); // массив объектов со свойствами name и price
