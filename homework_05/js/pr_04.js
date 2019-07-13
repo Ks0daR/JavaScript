2df9c2d54ad7d31ca43f9153277bcb1240a0c7b6
Ivan Shevchenko <rayserviz@gmail.com>
2019-07-13 22:20:29 +0300

    complete practical 05

---
+++ homework_05/js/pr_04.js
@@ -0,0 +1,38 @@
+/*
+ * Напиши конструктор Storage(items), который будет создавать объекты для управления складом товаров
+ * При вызове будет получать один аргумент - начальный массив товаров, и записываеть его в свойство items
+ *
+ * Добавь метод getItems, который возвращает массив текущих товаров
+ * Добавь метод addItem(item), который получает новый товар и добавляет его к текущим
+ * Добавь метод removeItem(item), который получет товар и, если он есть, удаляет его из текущих
+ */
+function Storage([...items]) {
+  this.items = items;
+  this.getItems = function () {
+    return this.items;
+  };
+  this.findItem = function (item) {
+    const findItems = this.items.indexOf(item);
+    return findItems;
+  };
+  this.addItem = function (item) {
+    const find = this.findItem(item);
+    if (find < 0) this.items.push(item);
+  };
+  this.removeItem = function (item) {
+    const find = this.findItem(item);
+    if (find >= 0) this.items.splice(find, 1);
+  };
+}
+
+const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор']);
+
+const items = storage.getItems();
+console.log(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+// storage.findItem('Нанитоиды');
+
+storage.addItem('Дроид');
+console.log(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.log(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
